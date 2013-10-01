f391fc780297c0e486f7ab44df994bb1bd31348b
Will Kurt <wckurt@gmail.com>
2013-10-01 00:01:23 -0700

    vectorized sws as well

--- ScrabbleScore/R/sws.R
+++ ScrabbleScore/R/sws.R
@@ -2,13 +2,13 @@ sws <-
 function(w,only.possible=TRUE,check.valid=FALSE){
   #Scrabble words have no case
   w <- tolower(w)
-  if(check.valid && !is.twl06.word(w)){
-    return(0)
-  }
-  wv <- strsplit(w,"")[[1]]
-  init.score <- sum(sapply(wv,sls))
+  wv <- strsplit(w,"")
+  init.score <- sapply(lapply(wv,sls),sum)
   if(only.possible){
     init.score <- init.score - impossible.points(wv)
+  }
+  if(check.valid){
+    init.score[!is.twl06.word(w)] <- 0
   }
   return(init.score)
 }
